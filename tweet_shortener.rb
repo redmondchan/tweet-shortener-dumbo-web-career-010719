786982e6519496918b9873a775808fe8ffc1bfe1
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 19:38:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,3 +29,20 @@ end
 def bulk_tweet_shortener(tweet)
   tweet.each {|str| puts word_substituter(str)}
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+    tweet[0..136]+ "..."
+  else
+    tweet
+  end
+end
