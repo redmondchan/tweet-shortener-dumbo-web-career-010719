ccacb51bad1d08682eecd7ffe647571d5ac61ed0
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 19:21:47 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def dictionary {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
+
+def word_substituter(str)
+  tweet = str.split("")
+  new_tweet = tweet.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  new_tweey.join("")
+end
