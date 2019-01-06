470a1de3eff0380aed70b90affda5dd3ea26d3c9
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 19:55:06 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,48 @@
 # Write your code here.
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+}
+end
+
+def word_substituter(str)
+   tweet = str.split(" ")
+   tweet.each_with_index do |word, index|
+      dictionary.each do |key, value|
+         if word.downcase == key
+            tweet[index] = value
+         end
+      end
+   end
+   tweet.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+   tweet.each { |str| puts word_substituter(str) }
+end
+
+def selective_tweet_shortener(tweet)
+   if tweet.length > 140
+      word_substituter(tweet)
+   else
+      tweet
+   end
+end
+
+def shortened_tweet_truncator(tweet)
+   if tweet.length > 140
+      word_substituter(tweet)
+      tweet[0..136] + "..."
+   else
+      tweet
+   end
+end
